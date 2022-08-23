d9cf85f0b4d99a40e23a1f5d4f53e1021d515ebe
DNYLU <dannylu1999@gmail.com>
2022-08-23 10:37:45 +0200

    Initial commit

--- src/main/java/com/example/gademo/GAdemoApplication.java
+++ src/main/java/com/example/gademo/GAdemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GAdemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GAdemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(GAdemoApplication.class, args);
+  }
 
 }
